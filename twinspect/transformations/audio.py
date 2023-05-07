32ab631e86cf8695150ec428f36565fd1557ad73
Titusz Pan <titusz.pan@gmail.com>
2023-05-07 18:33:04 +0200

    reformat audio transformations

--- twinspect/transformations/audio.py
+++ twinspect/transformations/audio.py
@@ -11,11 +11,11 @@ def trim(file_path: Path, seconds: float, position: str) -> Path:
     trim_duration = int(seconds * 1000)
 
     # Trim the audio based on the given position
-    if position == 'start':
+    if position == "start":
         trimmed_audio = audio[trim_duration:]
-    elif position == 'end':
+    elif position == "end":
         trimmed_audio = audio[:-trim_duration]
-    elif position == 'both':
+    elif position == "both":
         trimmed_audio = audio[trim_duration:-trim_duration]
     else:
         raise ValueError("Invalid position argument. Must be 'start', 'end', or 'both'.")
@@ -36,11 +36,11 @@ def fade(file_path: Path, seconds: int, position: str) -> Path:
     fade_duration = seconds * 1000
 
     # Apply fade in/out effect based on the given position
-    if position == 'in':
+    if position == "in":
         faded_audio = audio.fade_in(fade_duration)
-    elif position == 'out':
+    elif position == "out":
         faded_audio = audio.fade_out(fade_duration)
-    elif position == 'both':
+    elif position == "both":
         faded_audio = audio.fade_in(fade_duration).fade_out(fade_duration)
     else:
         raise ValueError("Invalid position argument. Must be 'in', 'out', or 'both'.")
@@ -64,28 +64,32 @@ def transcode(file_path: Path, codec: str, kbps: int) -> Path:
     """
     file_path = Path(file_path)
     output_format, file_extension, codec_profile = {
-        'mp3': ('libmp3lame', '.mp3', None),
-        'wav': ('pcm_s16le', '.wav', None),
-        'ogg': ('libvorbis', '.ogg', None),
-        'flac': ('flac', '.flac', None),
-        'aac': ('aac', '.m4a', 'aac_he_v2'),
+        "mp3": ("libmp3lame", ".mp3", None),
+        "wav": ("pcm_s16le", ".wav", None),
+        "ogg": ("libvorbis", ".ogg", None),
+        "flac": ("flac", ".flac", None),
+        "aac": ("aac", ".m4a", "aac_he_v2"),
     }.get(codec.lower(), (None, None, None))
 
     if output_format is None:
         raise ValueError(
-            "Invalid codec. Supported codecs are 'mp3', 'wav', 'ogg', 'flac', and 'aac'.")
+            "Invalid codec. Supported codecs are 'mp3', 'wav', 'ogg', 'flac', and 'aac'."
+        )
 
     new_file_name = f"z{file_path.stem}_transcoded-{codec}-{kbps}kbps{file_extension}"
     new_file_path = file_path.with_name(new_file_name)
 
     cmd = [
-        'ffmpeg',
-        '-y',
-        '-i', str(file_path),
-        '-vn',
-        '-c:a', output_format,
-        '-b:a', f'{kbps}k',
-        str(new_file_path)
+        "ffmpeg",
+        "-y",
+        "-i",
+        str(file_path),
+        "-vn",
+        "-c:a",
+        output_format,
+        "-b:a",
+        f"{kbps}k",
+        str(new_file_path),
     ]
 
     # Note: Disabled using aac_he_v2 profile
@@ -100,23 +104,25 @@ def transcode(file_path: Path, codec: str, kbps: int) -> Path:
 
 
 def compress(file_path: Path, intensity: str) -> Path:
-    if intensity.lower() not in ['low', 'medium', 'strong']:
+    if intensity.lower() not in ["low", "medium", "strong"]:
         raise ValueError("Intensity should be 'low', 'medium', or 'strong'.")
 
     settings = {
-        'low': {'attack': 20, 'release': 250, 'ratio': 2, 'threshold': -15},
-        'medium': {'attack': 10, 'release': 200, 'ratio': 3, 'threshold': -20},
-        'strong': {'attack': 5, 'release': 100, 'ratio': 4, 'threshold': -25},
+        "low": {"attack": 20, "release": 250, "ratio": 2, "threshold": -15},
+        "medium": {"attack": 10, "release": 200, "ratio": 3, "threshold": -20},
+        "strong": {"attack": 5, "release": 100, "ratio": 4, "threshold": -25},
     }[intensity.lower()]
 
     new_file_name = f"z{file_path.stem}_compressed-{intensity.lower()}{file_path.suffix}"
     new_file_path = file_path.with_name(new_file_name)
 
     cmd = [
-        'ffmpeg',
-        '-i', str(file_path),
-        '-af', f'acompressor=attack={settings["attack"]}:release={settings["release"]}:ratio={settings["ratio"]}:threshold={settings["threshold"]}dB',
-        str(new_file_path)
+        "ffmpeg",
+        "-i",
+        str(file_path),
+        "-af",
+        f'acompressor=attack={settings["attack"]}:release={settings["release"]}:ratio={settings["ratio"]}:threshold={settings["threshold"]}dB',
+        str(new_file_path),
     ]
 
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
@@ -130,12 +136,13 @@ def equalize(file_path: Path) -> Path:
     new_file_path = file_path.with_name(new_file_name)
 
     cmd = [
-        'ffmpeg',
-        '-y',
-        '-i', str(file_path),
-        '-af', "equalizer=f=1000:t=h:width=200:g=-3, highpass=f=100, lowpass=f=15000",
-
-        str(new_file_path)
+        "ffmpeg",
+        "-y",
+        "-i",
+        str(file_path),
+        "-af",
+        "equalizer=f=1000:t=h:width=200:g=-3, highpass=f=100, lowpass=f=15000",
+        str(new_file_path),
     ]
 
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
@@ -147,11 +154,13 @@ def echo(file_path: Path) -> Path:
     new_file_name = f"z{file_path.stem}_echoed{file_path.suffix}"
     new_file_path = file_path.with_name(new_file_name)
     cmd = [
-        'ffmpeg',
-        '-y',
-        '-i', str(file_path),
-        '-af', "aecho=0.8:0.88:60:0.4",
-        str(new_file_path)
+        "ffmpeg",
+        "-y",
+        "-i",
+        str(file_path),
+        "-af",
+        "aecho=0.8:0.88:60:0.4",
+        str(new_file_path),
     ]
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
     return new_file_path
@@ -162,11 +171,13 @@ def loudnorm(file_path: Path) -> Path:
     new_file_name = f"z{file_path.stem}_loudnormed{file_path.suffix}"
     new_file_path = file_path.with_name(new_file_name)
     cmd = [
-        'ffmpeg',
-        '-y',
-        '-i', str(file_path),
-        '-af', "loudnorm=I=-16:TP=-1.5:LRA=11",
-        str(new_file_path)
+        "ffmpeg",
+        "-y",
+        "-i",
+        str(file_path),
+        "-af",
+        "loudnorm=I=-16:TP=-1.5:LRA=11",
+        str(new_file_path),
     ]
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
     return new_file_path
