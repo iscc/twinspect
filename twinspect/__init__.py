45c122b470e6dd65c7765e7e62bdcd9abf67fa47
Titusz Pan <titusz.pan@gmail.com>
2023-05-07 18:27:02 +0200

    cleanup init

--- twinspect/__init__.py
+++ twinspect/__init__.py
@@ -1,43 +1,22 @@
 from pathlib import Path
-from loguru import logger
-from rich.console import Console
+
 
 __version__ = "0.1.0"
 
-console = Console()
-logger.remove()
-
-
-def _log_formatter(record: dict) -> str:
-    """Log message formatter"""
-    color_map = {
-        'TRACE': 'blue',
-        'DEBUG': 'cyan',
-        'INFO': 'bold',
-        'SUCCESS': 'bold green',
-        'WARNING': 'yellow',
-        'ERROR': 'bold red',
-        'CRITICAL': 'bold white on red'
-    }
-    lvl_color = color_map.get(record['level'].name, 'cyan')
-    return (
-        '[not bold green]{time:YYYY/MM/DD HH:mm:ss}[/not bold green] | {level.icon}'
-        + f'  - [{lvl_color}]{{message}}[/{lvl_color}]'
-    )
-
-
-logger.add(
-    console.print,
-    level='TRACE',
-    format=_log_formatter,
-    colorize=True,
-)
 
 CODE_DIR = Path(__file__).parent.parent.resolve().absolute()
 DEFAULT_ROOT_FOLDER = CODE_DIR / "data"
 DEFAULT_CONFIG_FILE = CODE_DIR / "config.yml"
 
-from twinspect.schema import Configuration, Dataset, Transformation, Mode, Algorithm, Task
+
+from twinspect.globals import *
+from twinspect.datasets.ultils import *
+from twinspect.benchmarks.hamming import *
+from twinspect.datasets.integrity import *
+from twinspect.datasets.download import *
+from twinspect.models import *
 from twinspect.options import *
 from twinspect.tools import *
-
+from twinspect.transformations.transform import *
+from twinspect.processing import *
+from twinspect.exceptions import *
