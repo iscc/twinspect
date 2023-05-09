4d8a916523adc9989aa759abafe1e89bc14abfd2
Titusz Pan <titusz.pan@gmail.com>
2023-05-09 22:56:18 +0200

    Add LameDuck hamming search

--- twinspect/metrics/hamming.py
+++ twinspect/metrics/hamming.py
@@ -14,10 +14,101 @@ from pathlib import Path
 from faiss import IndexBinaryHNSW, read_index_binary, write_index_binary
 
 
-__all__ = ["HammingHero"]
+__all__ = ["HammingHero", "LameDuck"]
 
 
-class HammingHero:
+class BaseHammingSearch:
+    def _load_csv(self):
+        # type: () -> NDArray[np.uint8]
+        """
+        Load codes from csv to numpy array and returns the numpy array.
+
+        :return: 2-dimensional numpy array of binary codes.
+        """
+        # Load codes from csv to numpy array
+        log.debug(f"Loading codes from {self.csv_path.name}")
+        with self.csv_path.open("r") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=";")
+            hex_codes = [row[self.code_field] for row in reader]
+
+        num_codes = len(hex_codes)
+        code_length = len(hex_codes[0]) // 2
+        uint8_matrix = np.empty((num_codes, code_length), dtype=np.uint8)
+        for i, hex_code in enumerate(hex_codes):
+            for j in range(0, len(hex_code), 2):
+                uint8_matrix[i, j // 2] = np.uint8(int(hex_code[j : j + 2], 16))
+
+        self.numpy_codes = uint8_matrix
+        return uint8_matrix
+
+
+class LameDuck(BaseHammingSearch):
+    """
+    Brute force all-pairs based exact NNS Search
+    """
+
+    def __init__(self, csv_path, code_field="code"):
+        # type: (Path|str, str) -> None
+        """
+        Cached initialization from a CSV-file which must have a hex coded comapct binary codes in
+        the `code_field` column.
+
+        :param csv_path: Path to the CSV file containing hex coded compact binary codes.
+        :param code_field: Column name containing the binary codes.
+        """
+        c = Path(csv_path)
+        self.csv_path = c
+        self.code_field = code_field
+        self.numpy_codes: NDArray[np.uint8] | None = None
+        self._load_csv()
+
+    def compute_queries(self, threshold):
+        # type: (int) -> pd.DataFrame
+        """
+        Collect query results into a DataFrame of the form:
+
+            id                                       query_result
+        0    0  [(0, 4), (0, 5), (0, 7), (0, 8), (0, 9), (1, 1...
+        1    1  [(1, 10), (2, 0), (2, 3), (2, 4), (2, 5), (2, ...
+
+        :param threshold: Hamming distance threshold for the search.
+        :return: DataFrame with ids and query results
+        """
+        query_results = list(self.iter_queries(threshold))
+        df = pd.DataFrame({"id": range(len(query_results)), "query_result": query_results})
+        return df
+
+    def iter_queries(self, threshold):
+        # type: (int) -> Iterable[NDArray[np.uint8]]
+        """
+        Iterate over all pairs query results with hamming `threshold`.
+
+        :param threshold: Hamming distance threshold for the search.
+        """
+        for i, code in enumerate(self.numpy_codes):
+            query_result = []
+            for j, other_code in enumerate(self.numpy_codes):
+                if i != j:
+                    distance = self.hamming_distance(code, other_code)
+                    if distance <= threshold:
+                        query_result.append((distance, j))
+            query_result.sort()
+            yield query_result
+
+    @staticmethod
+    def hamming_distance(code1, code2):
+        # type: (NDArray[np.uint8], NDArray[np.uint8]) -> int
+        """
+        Calculate the Hamming distance between two binary codes.
+
+        :param code1: Binary code 1.
+        :param code2: Binary code 2.
+        :return: Hamming distance.
+        """
+        return int(np.sum(np.unpackbits(code1 ^ code2)))
+
+
+class HammingHero(BaseHammingSearch):
     """
     Efficient ANNS Search Engine based on the Faiss library.
     """
@@ -106,25 +197,14 @@ class HammingHero:
         """
         write_index_binary(self.index, self.index_file.as_posix())
 
-    def _load_csv(self):
-        # type: () -> NDArray[np.uint8]
-        """
-        Load codes from csv to numpy array and returns the numpy array.
 
-        :return: 2-dimensional numpy array of binary codes.
-        """
-        # Load codes from csv to numpy array
-        log.debug(f"Loading codes from {self.csv_path.name}")
-        with self.csv_path.open("r") as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=";")
-            hex_codes = [row[self.code_field] for row in reader]
+if __name__ == "__main__":
+    from codetiming import Timer
 
-        num_codes = len(hex_codes)
-        code_length = len(hex_codes[0]) // 2
-        uint8_matrix = np.empty((num_codes, code_length), dtype=np.uint8)
-        for i, hex_code in enumerate(hex_codes):
-            for j in range(0, len(hex_code), 2):
-                uint8_matrix[i, j // 2] = np.uint8(int(hex_code[j : j + 2], 16))
-
-        self.numpy_codes = uint8_matrix
-        return uint8_matrix
+    fp = r"E:\twinspect\image_code_v0_256-pin_1000-f9ba7fb300ead3bf-simprint.csv"
+    hh = HammingHero(fp)
+    with Timer():
+        print(hh.compute_queries(8))
+    ld = LameDuck(fp)
+    with Timer():
+        print(ld.compute_queries(8))
